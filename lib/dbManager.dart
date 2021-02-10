09d477456da00322021c5caab216ea1654b29907
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2021-02-10 17:55:26 +0530

    add exception handling, error and usage alerts

--- lib/dbManager.dart
+++ lib/dbManager.dart
@@ -1,12 +1,12 @@
 import 'package:sqlite3/sqlite3.dart';
 import 'package:timeago/timeago.dart' as timeago;
-import 'package:tdl/settingsManager.dart';
 import 'package:path/path.dart' as path;
 import 'dart:io' as io;
-// local dependencies
+// local dependencies ----
+import 'package:tdl/settingsManager.dart';
 
+// constants
 String homeDirPath = path.absolute(io.Platform.environment['HOME']);
-
 String dbLocation = path.join(homeDirPath, '.tdl/db/todoLists');
 // db setup
 
@@ -19,7 +19,8 @@ void initDB() {
 }
 
 void createList(String listName) {
-  db.execute('''
+  try {
+    db.execute('''
     CREATE TABLE IF NOT EXISTS ${listName} (
       id INTEGER NOT NULL PRIMARY KEY,
       todo TEXT NOT NULL,
@@ -27,58 +28,106 @@ void createList(String listName) {
       createdAt INTEGER NOT NULL
     );
   ''');
+  } catch (err) {
+    print("ERROR : ${err.message}");
+  }
 }
 
 void deleteList(String listName) {
-  db.execute('DROP TABLE ${listName}');
+  try {
+    db.execute('DROP TABLE ${listName}');
+  } on SqliteException catch (e) {
+    if (e.extendedResultCode >= 1 && e.extendedResultCode <= 8) {
+      print(
+          '\nERROR: Got error while reading the local Database.\nPlease re-check your command for misspelled names\n');
+    }
+  } catch (err) {
+    print("ERROR : ${err.message}");
+  }
 }
 
 // adds a todo to a particular list
 void addTodo(String todoText, String listName) {
-  var now = DateTime.now().microsecondsSinceEpoch;
-  db.execute('''
+  try {
+    var now = DateTime.now().microsecondsSinceEpoch;
+    db.execute('''
     INSERT INTO ${listName}
     (todo,createdAt)
     VALUES
     ("${todoText}",${now.toString()});
   ''');
+    print("Added one todo in ${listName}");
+  } on SqliteException catch (e) {
+    if (e.extendedResultCode >= 1 && e.extendedResultCode <= 8) {
+      print(
+          '\nERROR: Got error while reading the local Database.\nPlease re-check your command for misspelled names\n');
+    }
+  } catch (err) {
+    print("ERROR : ${err.message}");
+  }
 }
 
 // removes a todo to a particular list
 void removeTodo(String todoId, String listName) {
-  // find actual todo-id from db
-  final notDoneTodos = db.select(
-      'SELECT * FROM ${listName} WHERE done == 0 ORDER BY createdAt ASC LIMIT ${todoId}');
-  String actualTodoId =
-      notDoneTodos.elementAt(int.parse(todoId) - 1)['id'].toString();
-  // db operation
-  db.execute('''
+  try {
+    // find actual todo-id from db
+    final notDoneTodos = db.select(
+        'SELECT * FROM ${listName} WHERE done == 0 ORDER BY createdAt ASC LIMIT ${todoId}');
+    String actualTodoId =
+        notDoneTodos.elementAt(int.parse(todoId) - 1)['id'].toString();
+    // db operation
+    db.execute('''
     DELETE FROM ${listName}
     WHERE id == ${actualTodoId};
   ''');
+    print("Removed one todo from ${listName}");
+  }
+  // error handeling
+  on RangeError catch (e) {
+    print(
+        '\nERROR: Got error while reading the local Database.\nPlease re-check your given todo-id\n');
+  } on SqliteException catch (e) {
+    if (e.extendedResultCode >= 1 && e.extendedResultCode <= 8) {
+      print(
+          '\nERROR: Got error while reading the local Database.\nPlease re-check your command for misspelled names\n');
+    }
+  } catch (err) {
+    print("ERROR : ${err.message}");
+  }
 }
 
 // lists todos from a particular list
 void listTodos(String listName) {
-  final notDoneTodos = db.select(
-      'SELECT * FROM ${listName} WHERE done == 0 ORDER BY createdAt ASC');
-  print(listName + ' ---------------------------');
-  print('Not Done : ' + notDoneTodos.length.toString());
+  try {
+    final notDoneTodos = db.select(
+        'SELECT * FROM ${listName} WHERE done == 0 ORDER BY createdAt ASC');
+    print(listName + ' ---------------------------');
+    print('Not Done : ' + notDoneTodos.length.toString());
 
-  var count = 1;
-  notDoneTodos.forEach((element) {
-    var createdAt = DateTime.fromMicrosecondsSinceEpoch(element['createdAt']);
-    var dateString = timeago.format(createdAt);
-    print('${count} ${element['todo']} : ${dateString}');
-    count++;
-  });
+    var count = 1;
+    notDoneTodos.forEach((element) {
+      var createdAt = DateTime.fromMicrosecondsSinceEpoch(element['createdAt']);
+      var dateString = timeago.format(createdAt);
+      print('${count} ${element['todo']} : ${dateString}');
+      count++;
+    });
+  }
+  // error handeling
+  on SqliteException catch (e) {
+    if (e.extendedResultCode >= 1 && e.extendedResultCode <= 8) {
+      print(
+          '\nERROR: Got error while reading the local Database.\nPlease re-check your command for misspelled names\n');
+    }
+  } catch (err) {
+    print("ERROR : ${err.message}");
+  }
 }
 
 // list all tables
 void listTables() {
   final allLists =
       db.select("SELECT name FROM sqlite_master WHERE type='table';");
-  print('Total ${allLists.length} Lists');
+  print('Found Total ${allLists.length} Lists\n');
   allLists.forEach((table) {
     print(table['name']);
   });
